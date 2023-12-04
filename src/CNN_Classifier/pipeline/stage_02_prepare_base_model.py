219e0e3541a6fecaf568a5c20bc691c5263c1557
Karthiknn10 <karthiknn10@gmail.com>
2023-12-04 18:50:11 +0530

    Adding model training

--- src/CNN_Classifier/pipeline/stage_02_prepare_base_model.py
+++ src/CNN_Classifier/pipeline/stage_02_prepare_base_model.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from CNN_Classifier.config.configuration import ConfigurationManager
 from CNN_Classifier.components.prepare_base_model import PrepareBaseModel
 from CNN_Classifier import logger
@@ -7,7 +8,11 @@ STAGE_NAME = "Prepare base model"
 
 class PrepareBaseModelTrainingPipeline:
     def __init__(self):
-        pass
+        # Avoid OOM errors by setting GPU Memory Consumption Growth
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        for gpu in gpus: 
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logger.info(f"GPU for training: {tf.config.list_physical_devices('GPU')}")
 
     def main(self):
         config = ConfigurationManager()
